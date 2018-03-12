2c12a43596dce76ff8403545a83ef3233daa8c1b
anika-mukherji <anika.mukherji@duke.edu>
2018-03-12 13:42:01 -0400

    adding structure for requests file

---
+++ requests.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route("/api/heart_rate", methods=["POST"])
+def heart_rate_post():
+    pass
+
+
+@app.route("/api/heart_rate/<user_email>", methods=["GET"])
+def heart_rate_get():
+    pass
+
+
+@app.route("/api/heart_rate/average/<user_email>", methods=["GET"])
+def heart_rate_average():
+    pass
+
+
+@app.route("/api/heart_rate/interval_average", methods=["POST"])
+def heart_rate_interval_average():
+    pass
