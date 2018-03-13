92c1d3855faa956fde1acd20d950828b4cd40793
anika-mukherji <anika.mukherji@duke.edu>
2018-03-13 18:46:18 -0400

    average hr test

--- models_test.py
+++ models_test.py
@@ -1,6 +1,7 @@
 from pymodm import connect
 from models import User
 from main import *
+import datetime
 
 
 def test_user():
@@ -31,4 +32,10 @@ def test_average_hr():
     except ImportError as e:
         print("Necessary import failed: {}".format(e))
         return
-    pass
+    connect("mongodb://localhost:27017/heart_rate_app")
+    create_user("test1@test.test", age=0, hr=1)
+    u = models.User.objects.raw({"_id": "test@test.test"}).first()
+    assert u.average_hr() == 1
+    add_heart_rate("test1@test.test", heart_rate=3,
+                    time=datetime.datetime.now())
+    assert u.average_hr() == 2.0
