2f602cbf44a738e0148af22c25318783208de819
anika-mukherji <anika.mukherji@duke.edu>
2018-03-15 16:31:33 -0400

    test for is_tachycardic

--- test_models.py
+++ test_models.py
@@ -84,3 +84,56 @@ def test_adjust_age():
         with pytest.raises(ValueError):
             u.age_units = i
             u.adjust_age()
+
+
+def test_is_tachycardic():
+    try:
+        import pytest
+        import datetime
+        from models import User
+        import time
+    except ImportError as e:
+        print("Necessary import failed: {}".format(e))
+        return
+    d = datetime.datetime.now()
+    u = User("test@test.test", age=1, age_units="day")
+    assert u.is_tachycardic(160) is True
+    assert u.is_tachycardic(150) is False
+    u.age = 5
+    assert u.is_tachycardic(170) is True
+    assert u.is_tachycardic(150) is False
+    u.age = 1
+    u.age_units = "week"
+    assert u.is_tachycardic(185) is True
+    assert u.is_tachycardic(180) is False
+    u.age_units = "month"
+    assert u.is_tachycardic(180) is True
+    assert u.is_tachycardic(178) is False
+    u.age = 5
+    assert u.is_tachycardic(188) is True
+    assert u.is_tachycardic(178) is False
+    u.age = 10
+    assert u.is_tachycardic(170) is True
+    assert u.is_tachycardic(168) is False
+    u.age = 1
+    u.age_units = "year"
+    assert u.is_tachycardic(160) is True
+    assert u.is_tachycardic(150) is False
+    u.age = 3
+    assert u.is_tachycardic(140) is True
+    assert u.is_tachycardic(130) is False
+    u.age = 7
+    assert u.is_tachycardic(135) is True
+    assert u.is_tachycardic(125) is False
+    u.age = 10
+    assert u.is_tachycardic(132) is True
+    assert u.is_tachycardic(128) is False
+    u.age = 12
+    assert u.is_tachycardic(120) is True
+    assert u.is_tachycardic(115) is False
+    u.age = 16
+    assert u.is_tachycardic(101) is True
+    assert u.is_tachycardic(99) is False
+    u.age = 55
+    assert u.is_tachycardic(101) is True
+    assert u.is_tachycardic(99) is False
