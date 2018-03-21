4537e600f998ef2a8da30c65b67527ac900d0e19
anika-mukherji <anika.mukherji@duke.edu>
2018-03-21 13:43:45 -0400

    little fixes

--- requests_test.py
+++ requests_test.py
@@ -5,7 +5,7 @@ import requests
 if __name__ == "__main__":
     connect("mongodb://localhost:27017/heart_rate_app")
     u_dict = {"user_email": "anika@anika.com", "user_age": 20,
-              "age_units":"week", "heart_rate": 65}
+              "age_units": "week", "heart_rate": 65}
     r = requests.post("http://localhost:5000/api/heart_rate", json=u_dict)
     print(r.json())
     r2 = requests.get("http://localhost:5000/api/heart_rate/anika@anika.com")
@@ -15,5 +15,6 @@ if __name__ == "__main__":
     print(r3.json())
     r4 = requests.get("http://localhost:5000/api/heart_rate/anika@anika.com")
     print(r4.json())
-    r5 = requests.get("http://localhost:5000/api/heart_rate/average/anika@anika.com")
+    r5 = requests.get("http://localhost:5000/api/heart_rate"
+                      "/average/anika@anika.com")
     print(r5.json())
